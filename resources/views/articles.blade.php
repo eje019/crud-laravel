f2f71da01ec0868e6dfb447420e68e56b2f27c27
eje019 <emricndayake@gmail.com>
2026-03-23 16:24:34 +0100

    feat : ajout der la vue et la route pour la liste des articles

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mes Articles</title>
+    <!-- On inclut Bootstrap via un CDN (c'est une URL qui charge le CSS) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>Bienvenue sur la liste des articles</h1>
+        <p>Ici, on va bientôt lister les articles !</p>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('articles');
+});
+
+
+// route pour la liste des articles
+Route::get("/articles", function(){
+    return view("article");
 });
